f9e67f3e64cf9bd1ccdbc974bc5e485cc98b2827
Lukas <lukas.ancikevicius@gmail.com>
2021-08-11 14:38:36 +0300

    static concept type

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ import UserContext from './context/UserContext';
 
 class App extends Component {
   state = {
-    userName: 'Bob',
+    user: {
+      userName: 'Bob',
+      age: 41,
+    },
   };
   render() {
     return (
-      <UserContext.Provider value={this.state.userName}>
+      <UserContext.Provider value={this.state.user}>
         <div className="App">
           <MoviePage />
         </div>
--- src/context/MovieList.jsx
+++ src/context/MovieList.jsx
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import UserContext from './UserContext';
 class MovieList extends Component {
-  state = {};
+  // pridedam contexta kad butu pasiekiamas visoje klaseje(2 budas)
+  static contextType = UserContext;
+  componentDidMount() {
+    console.log('MovieList mounted to dom');
+    console.log('context', this.context);
+  }
   render() {
     return (
       <UserContext.Consumer>
-        {() => (
+        {(userValue) => (
           <div>
             <h4>Movie List</h4>
+            <p>
+              {userValue.userName} likes this movie list and he is{' '}
+              {userValue.age} years old
+            </p>
           </div>
         )}
       </UserContext.Consumer>
     );
   }
 }
-
+// pridedam contexta kad butu pasiekiamas visoje klaseje
+// MovieList.contextType = UserContext;
 export default MovieList;
