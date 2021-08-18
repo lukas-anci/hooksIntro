65ebcd3ead629c4f879578f143170ad30b4120c3
Lukas <lukas.ancikevicius@gmail.com>
2021-08-18 09:44:24 +0300

    add todo works, no visual

--- src/components/ReducerCounter/ReducerSimpleTodo.jsx
+++ src/components/ReducerCounter/ReducerSimpleTodo.jsx
@@ -1,14 +1,49 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 
-const ACTION = {};
+const ACTION = {
+  ADD_TODO: 'add-todo',
+};
 
-function countReducer(state, action) {}
+function todoReducer(todosArr, action) {
+  switch (action.type) {
+    case ACTION.ADD_TODO:
+      return [...todosArr, newTodo(action.payload)];
+
+    default:
+      return todosArr;
+  }
+}
+function newTodo(newTodoTitle) {
+  return {
+    id: Date.now(),
+    title: newTodoTitle,
+    done: false,
+  };
+}
 
 export default function ReducerSimpleTodo() {
+  const [todosArr, dispatch] = useReducer(todoReducer, []);
+  const [title, setTitle] = useState('');
+
+  function todoSubmitHandler(e) {
+    e.preventDefault();
+    if (title.trim().length === 0) return;
+    console.log(title);
+    dispatch({ type: ACTION.ADD_TODO, payload: title });
+    setTitle('');
+  }
+
   return (
-    <div>
+    <form onSubmit={todoSubmitHandler}>
       <h4>Reducer Simple Todo</h4>
-      <input type="text" placeholder="Enter todo" />
-    </div>
+      <input
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+        type="text"
+        placeholder="Enter todo"
+      />
+    </form>
   );
 }
